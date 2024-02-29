7987908ee758be40edafa22457f7f3affc51e1e1
VadymMurzin <prometey736@gmail.com>
2024-02-29 18:43:23 +0100

    Clean code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,41 +2,6 @@ import Contact from '../Contact/Contact';
 import css from './contactList.module.css';
 import { useSelector } from 'react-redux';
 
-// export default function ContactList() {
-//   const contacts = useSelector(state => state.contacts);
-
-//   const filteredContacts = Array.isArray(contacts)
-//     ? contacts.filter(contact => {
-//         return contact.name;
-//       })
-//     : [];
-
-//   return (
-//     <ul className={css.container}>
-//       {filteredContacts.map(item => (
-//         <Contact key={item.id} contact={item} />
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default function ContactList() {
-//   const contacts = useSelector(state => state.contacts);
-//   const filters = useSelector(state => state.filter.text); // Доступ до властивості text
-//   const filteredContacts = Array.isArray(contacts)
-//     ? contacts.filter(contact => {
-//         return contact.name.toLowerCase().includes(filters.toLowerCase()); // Використовуйте filters.text для фільтрації
-//       })
-//     : [];
-//   return (
-//     <ul className={css.container}>
-//       {filteredContacts.map(item => (
-//         <Contact key={item.id} contact={item} />
-//       ))}
-//     </ul>
-//   );
-// }
-
 export default function ContactList() {
   const contacts = useSelector(state => state.contacts);
   const filters = useSelector(state => state.filter.text);
@@ -44,22 +9,6 @@ export default function ContactList() {
     return (contact.name && contact.name.toLowerCase().includes(filters.toLowerCase()));
   });
 
-
-//   console.log('Contacts:', contacts);
-// console.log('Filters:', filters);
-// console.log('All Contacts:', contacts);
-
-  // const filteredContacts = Array.isArray(contacts)
-  // ? contacts.filter(contact => {
-  //     return (
-  //       contact.name &&
-  //       filters &&
-  //       contact.name.toLowerCase().includes(filters.toLowerCase())
-  //     );
-  //   })
-  // : [];
-
-  // console.log('Filtered Contacts:', filteredContacts);
 
   return (
     <ul className={css.container}>
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,30 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// const filterSlice = createSlice({
-//   name: 'filters',
-//   initialState: '',
-//   reducers: {
-//     filterFiltration: (state, action) => {
-//       return state, action.payload;
-//     },
-//   },
-// });
-
-
 const filterSlice = createSlice({
   name: 'filters',
-  initialState: { text: '' }, // Використовуйте об'єкт із властивістю text
+  initialState: { text: '' },
   reducers: {
     filterFiltration: (state, action) => {
-      // state.text = action.payload; // Оновлюйте властивість text
-      // return { text: action.payload };
       return { ...state, text: action.payload };
-      // return Object.assign({}, state, { text: action.payload });
     },
   },
 });
-
-
 
 export const { filterFiltration } = filterSlice.actions;
 export default filterSlice.reducer;
